d837fab2aab185728d51f8d241a3b522f6eef653
Travis Hoppe <travis.hoppe@gmail.com>
2016-01-04 15:40:52 -0500

    Add fabfile for easy deployment

--- fabfile.py
+++ fabfile.py
@@ -2,3 +2,12 @@ from fabric.api import local
 
 def push():
     local('git commit -a')
+    local('git push')
+
+def test():
+    clean()
+    local("python w2v_pipeline/import_data.py")
+    local("python w2v_pipeline/phrases_from_abbrs.py")
+
+def clean():
+    local("rm -rf sql_data collated")
