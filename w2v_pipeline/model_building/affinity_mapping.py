55b972920133eb270e8055113306af41fa3ea06c
Travis Hoppe <travis.hoppe@gmail.com>
2016-03-07 10:08:56 -0500

    Updated affinity mapping to capture local index

--- w2v_pipeline/model_building/affinity_mapping.py
+++ w2v_pipeline/model_building/affinity_mapping.py
@@ -23,6 +23,9 @@ def compute_affinity(item):
     local_counts = collections.Counter(valid_tokens)
     labels = np.array(list(set(valid_tokens)))
 
+    token_clf_index = np.array([M.word2index[w]
+                                for w in valid_tokens])
+
     if not labels.size:
         msg = "Document has no valid tokens! This is problem."
         raise ValueError(msg)
@@ -46,15 +49,15 @@ def compute_affinity(item):
         distance_block = DV[idx,:][:,idx]
         vector_block = V[idx,:]
 
-
         average_vector  = vector_block.sum(axis=0)
         average_vector /= np.linalg.norm(average_vector)
-
+        
         data[i] = {
             "average_vector":average_vector,
             "intra_mean":distance_block.mean(),
             "intra_std" :distance_block.std(),
             "size":vector_block.shape[0],
+            "token_clf_index":token_clf_index[idx],
         }
 
     return f_idx, f_sql, data
@@ -80,56 +83,14 @@ class affinity_mapping(corpus_iterator):
         damping = self.damping
         M = self.M
 
-    def compute_affinity(self, item):
-        
-        text,f_idx,f_sql = item
-        tokens = text.split()
-
-        # Find out which tokens are defined
-        valid_tokens = [w for w in tokens if w in self.M]
-        local_counts = collections.Counter(valid_tokens)
-        labels = np.array(list(set(valid_tokens)))
-
-        if not labels.size:
-            msg = "Document has no valid tokens! This is problem."
-            raise ValueError(msg)
-
-        V  = np.array([self.M[w] for w in labels])
-        DV = cdist(V,V,metric='cosine')
-
-        # Values are sometimes "slightly" less than zero due to rounding
-        DV[DV<0] = 0
-
-        cluster_args = {"damping":self.damping}
-        cluster = cluster_clf(**cluster_args)
-        y_labels = cluster.fit_predict(DV)
-       
-        data = {}
-        
-        for i in sorted(np.unique(y_labels)):
-            idx = y_labels==i
-            
-            distance_block = DV[idx,:][:,idx]
-            vector_block = V[idx,:]
-
-
-            average_vector  = vector_block.sum(axis=0)
-            average_vector /= np.linalg.norm(average_vector)
-
-            data[i] = {
-                "average_vector":average_vector,
-                "intra_mean":distance_block.mean(),
-                "intra_std" :distance_block.std(),
-                "size":vector_block.shape[0],
-            }
-
-        return f_idx, f_sql, data
-
+        vocab_n = len(M.index2word)
+        M.word2index = dict([(w,i) for w,i in
+                             zip(M.index2word,range(vocab_n))])
 
     def compute(self, config):
 
         func = compute_affinity
-        
+
         if self._PARALLEL:
             import multiprocessing
             MP = multiprocessing.Pool()
@@ -179,6 +140,12 @@ class affinity_grouping(corpus_iterator):
         
         size = np.hstack([g[name]["size"] for name in g])
         mu   = np.hstack([g[name]["mu"] for name in g])
+
+        print mu
+        import seaborn as sns
+        sns.distplot(mu)
+        sns.plt.show()
+        
         
         idx  = (size>=5) * (size<=10) * (mu < 0.65)
         #idx  = (size>=5) * (size<=10) * (mu < 0.5)
@@ -204,7 +171,10 @@ class affinity_grouping(corpus_iterator):
             affinity_V.append(v)
 
         affinity_V = np.array(affinity_V)
-        
+
+        if "affinity_vectors" in self.h5:
+            del self.h5["affinity_vectors"]
+            
         self.h5["affinity_vectors"] = affinity_V
 
     def save(self, config, result):
