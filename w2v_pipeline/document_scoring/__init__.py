cb2e734680160320a5422857c2d7c36b42682d3b
Travis Hoppe <travis.hoppe@gmail.com>
2016-05-19 17:33:18 -0400

    Add term_document_freq to score functions

--- w2v_pipeline/document_scoring/__init__.py
+++ w2v_pipeline/document_scoring/__init__.py
@@ -1,9 +1,10 @@
-from .term_frequency import term_frequency
+from .term_frequency import term_frequency, term_document_frequency
 from .document_scores import document_scores
 from .affinity_mapping import affinity_mapping, affinity_grouping, affinity_scoring
 
 __all__ = [
     'term_frequency',
+    'term_document_frequency',
     'document_scores',
     'affinity_mapping',
     'affinity_grouping',
