9dfaee121ed29176504ecbe24ec05302bec196a1
thoppe <travis.hoppe@gmail.com>
2016-08-08 10:14:34 -0400

    Fixed mismatching paren bug

--- w2v_pipeline/preprocessing/remove_parenthesis.py
+++ w2v_pipeline/preprocessing/remove_parenthesis.py
@@ -1,4 +1,5 @@
 import pyparsing as pypar
+import pattern.en
 
 class remove_parenthesis(object):
     def __init__(self):
@@ -16,23 +17,44 @@ class remove_parenthesis(object):
 
         g = pypar.OneOrMore(word | nest_grammar)
         self.grammar = g
+        
+        #self.parse = lambda x:pattern.en.parse(x,chunks=False,tags=False)
+        self.parse = lambda x:pattern.en.tokenize(x)#,chunks=False,tags=False)
 
                                         
-    def __call__(self,line):
-        try:
-            tokens = self.grammar.parseString(line)
-        except (pypar.ParseException, RuntimeError):
-            # On fail simply remove all parens
-            line = line.replace('(','')
-            line = line.replace(')','')
-            line = line.replace('[','')
-            line = line.replace(']','')
-            line = line.replace('{','')
-            line = line.replace('}','')
-            tokens = line.split()
+    def __call__(self,text):
 
-        # Remove nested parens
-        tokens = [x for x in tokens if type(x) in [str,unicode]]
-        text = ' '.join(tokens)
-        return text
+        sentences = self.parse(text)
 
+        doc_out = []
+        for sent in sentences:
+
+            # Count the number of left and right parens
+            LP = sum(1 for a in sent if a=='(')
+            RP = sum(1 for a in sent if a==')')
+
+            # If the count of the left paren doesn't match the right ignore
+            FLAG_valid = (LP == RP)
+
+            try:
+                tokens = self.grammar.parseString(sent)
+            except (pypar.ParseException, RuntimeError):
+                FLAG_valid = False
+
+            if not FLAG_valid:            
+                # On fail simply remove all parens
+                sent = sent.replace('(','')
+                sent = sent.replace(')','')
+                sent = sent.replace('[','')
+                sent = sent.replace(']','')
+                sent = sent.replace('{','')
+                sent = sent.replace('}','')
+                tokens = sent.split()
+
+            # Remove nested parens
+            tokens = [x for x in tokens if type(x) in [str,unicode]]
+            text = ' '.join(tokens)
+
+            doc_out.append(text)
+
+        return '\n'.join(doc_out)
