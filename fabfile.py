d015d0cea7f52810126cacb27cc71d8c317be597
thoppe <travis.hoppe@gmail.com>
2017-01-25 17:37:25 -0500

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,41 +1,21 @@
 from fabric.api import local
 
-target = "w2v_pipeline/predict.py"
-
-
-def push():
-    local('git commit -a')
-    local('git push')
-
-
-def run():
-    local("python {}".format(target))
-
-
-def edit():
-    local("emacs {} &".format(target))
-
-
-def view():
-    # local("sqlitebrowser data_sql/PLoS_bio.sqlite")
-    local("sqlitebrowser data_parsed/PLoS_bio.sqlite")
-
 
 def import_data():
     local("python w2v_pipeline/import_data.py")
     local("python w2v_pipeline/phrases_from_abbrs.py")
 
 
-def score():
-    local("python w2v_pipeline/score.py")
+def parse():
+    local("python w2v_pipeline/parse.py")
 
 
 def embed():
     local("python w2v_pipeline/embed.py")
 
 
-def parse():
-    local("python w2v_pipeline/parse.py")
+def score():
+    local("python w2v_pipeline/score.py")
 
 
 def predict():
