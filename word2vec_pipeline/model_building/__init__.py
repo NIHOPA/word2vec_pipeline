e02a2f06f19fc1aecc82ff80aeb0780a6356b993
Travis Hoppe <travis.hoppe@gmail.com>
2018-06-28 10:13:16 -0400

    Still working on hyperbolic embedding, checking in now

--- word2vec_pipeline/model_building/__init__.py
+++ word2vec_pipeline/model_building/__init__.py
@@ -1,7 +1,9 @@
 from .w2v_embedding import w2v_embedding
 from .d2v_embedding import d2v_embedding
+from .hyper_embedding import hyperbolic_embedding
 
 __all__ = [
     'w2v_embedding',
     'd2v_embedding',
+    'hyperbolic_embedding',
 ]
