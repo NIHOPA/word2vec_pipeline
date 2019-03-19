3121632a278e0b2ca18a40eb993be535b5d0dd93
thoppe <travis.hoppe@gmail.com>
2019-03-19 10:42:34 -0400

    Started conversion

---
+++ word2vec_pipeline/_version.py
@@ -0,0 +1,7 @@
+# One canonical source for the version number,
+# Versions should comply with PEP440.
+
+__version__ = "1.0.0"
+
+# Version tracking started in 1.0.0
+# Prior versions used older NLPre
